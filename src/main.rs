3776bcd4557a4208a7b4026c03e19f2b8a7f174f
zhangyemengren <624593758@qq.com>
2024-11-07 19:14:09 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
-use std::io;
+use std::{sync::Arc, time::{Duration, Instant}};
+use tokio::sync::Semaphore;
+use reqwest::ClientBuilder;
+// 并行请求的最大数量
+// qps
+// get 请求
+// 秒数
+// tui
+// 获取参数
 
-use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
-};
+const MAX_CONCURRENT_REQUESTS: usize = 4;
+const REQUEST_TIMEOUT: u64 = 1;
+const REQUEST_DURATION_SECS: u64 = 2;
 
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    terminal.clear()?;
-    let app_result = run(terminal);
-    ratatui::restore();
-    app_result
-}
+#[tokio::main]
+async fn main() {
+    let now = Instant::now();
+    let client = ClientBuilder::new().timeout(Duration::from_secs(REQUEST_TIMEOUT)).build().unwrap();
+    let client = Arc::new(client);
+    let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_REQUESTS));
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    println!("over");
     loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
+        if now.elapsed().as_secs() >= REQUEST_DURATION_SECS {
+            break;
         }
+        let client = Arc::clone(&client);
+        let semaphore = Arc::clone(&semaphore);
+        tokio::spawn(async move{
+            let _permit = semaphore.acquire().await.unwrap();
+            let res = client.get("http://localhost:8000")
+                .send()
+                .await
+                .unwrap();
+            println!("Status: {}", res.text().await.unwrap());
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            drop(_permit);
+        });
     }
+    println!("Done!");
 }
